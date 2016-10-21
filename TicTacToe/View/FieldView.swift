9cb52c66d7f67cdf678938688043e3a592cbc069
Jake Zeal <jake@jakezeal.com>
2016-10-21 11:50:35 -0700

    add FieldView initializer

--- TicTacToe/View/FieldView.swift
+++ TicTacToe/View/FieldView.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class FieldView: UIView {
 
+    let xPosition: Int // range: [0, 1, 2]
+    let yPosition: Int // range: [0, 1, 2]
     
+    init(frame: CGRect, xPosition: Int, yPosition: Int) {
+        
+        assert(xPosition >= 0 && xPosition <= 2, "value '\(xPosition)' is not allowed for x position of field")
+        assert(yPosition >= 0 && yPosition <= 2, "value '\(yPosition)' is not allowed for y position of field")
+        
+        self.xPosition = xPosition
+        self.yPosition = yPosition
+        
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
+
+

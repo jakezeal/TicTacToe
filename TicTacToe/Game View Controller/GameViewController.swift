6fe20688e7e69858e300c1097c84bdc48ab04ba0
Jake Zeal <jake@jakezeal.com>
2016-10-21 10:55:22 -0700

    rename ViewController to GameViewController

--- TicTacToe/Game View Controller/GameViewController.swift
+++ TicTacToe/Game View Controller/GameViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GameViewController.swift
 //  TicTacToe
 //
 //  Created by Jake on 10/21/16.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 

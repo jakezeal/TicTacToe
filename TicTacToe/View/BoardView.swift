69e7beeefb09631354a0557bc7d98947a7bebd83
Jake Zeal <jake@jakezeal.com>
2016-10-21 11:42:18 -0700

    add initial initializers to BoardView

--- TicTacToe/View/BoardView.swift
+++ TicTacToe/View/BoardView.swift
@@ -8,6 +8,28 @@
 
 import UIKit
 
+// MARK: - Private Properties
+fileprivate let numberOfRows: CGFloat = 3
+fileprivate let numberOfColumns: CGFloat = 3
+
 class BoardView: UIView {
+    
+    // MARK: - Initializers
+    convenience init(boardWidth: CGFloat, boardHeight: CGFloat) {
+        let fieldWidth = boardWidth / numberOfRows
+        let fieldHeight = boardHeight / numberOfColumns
+        self.init(fieldWidth: fieldWidth, fieldHeight: fieldHeight)
+    }
+    
+    init(fieldWidth: CGFloat, fieldHeight: CGFloat) {
+        let boardFrame = CGRect(x: 0, y: 0, width: fieldWidth * numberOfRows, height: fieldHeight * numberOfColumns)
+        super.init(frame: boardFrame)
+        
+        // Add fields
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
 }

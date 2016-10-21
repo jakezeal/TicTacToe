e8a9b16ccce5cb142ba02453ea254b34f04138f3
Jake Zeal <jake@jakezeal.com>
2016-10-21 11:03:09 -0700

    add BoardView

--- TicTacToe/Game View Controller/GameViewController.swift
+++ TicTacToe/Game View Controller/GameViewController.swift
@@ -12,7 +12,8 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    
     }
 
 }
---
+++ TicTacToe/View/BoardView.swift
@@ -0,0 +1,13 @@
+//
+//  BoardView.swift
+//  TicTacToe
+//
+//  Created by Jake on 10/21/16.
+//  Copyright © 2016 Jake. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+    
+}

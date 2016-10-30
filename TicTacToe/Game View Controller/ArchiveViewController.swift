da6e4736cb388ce7a0ec28529753119ba9e504d7
Jake Zeal <jake@jakezeal.com>
2016-10-29 20:58:04 -0700

    add ArchiveViewController

---
+++ TicTacToe/Game View Controller/ArchiveViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ArchiveViewController.swift
+//  TicTacToe
+//
+//  Created by Jake on 10/29/16.
+//  Copyright © 2016 Jake. All rights reserved.
+//
+
+import UIKit
+
+class ArchiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
